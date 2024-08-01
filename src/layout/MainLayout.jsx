7e9eda8c417986a3c2863fb7c2749345288d2c2c
madiinemm <uraimovamadina179@gmail.com>
2024-08-02 01:16:13 +0500

    quiz app completed

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,21 @@
+// react router dom imports
+import { Outlet } from "react-router-dom";
+
+// component
+import Navbar from "../components/Navbar";
+
+function MainLayout() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <footer>
+    
+      </footer>
+    </>
+  );
+}
+
+export default MainLayout;
